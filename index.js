1dea4ab351efa23c54654cac40240138c07c90e6
Charnell Dolley <cdolley516@gmail.com>
2018-07-29 21:57:32 -0400

    adding code challenge js

---
+++ index.js
@@ -0,0 +1,8 @@
+const name1 = "Melody Fallon";
+const name2 = "Mackenzie Freeman";
+const longerName = " has the longer name.";
+
+if (name1.length > name2.length) {
+    console.log(name1 + longerName)
+} else {}
+console.log(name2 + longerName);
